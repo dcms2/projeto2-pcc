aaa99fa39832fba693602863d810626902502ae1
Mateus Moury Fernandes da Rosa Borges <mmfrb@cin.ufpe.br>
2015-02-19 09:31:39 -0300

    values of an array changing

--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+char A[1000];
+
+bool cmp(int a, int b) {
+    return strcmp(A+a, A+b) < 0;
+}
+
 class SuffixArray {
 private:
     int* sa;
@@ -10,29 +16,43 @@ private:
     int* Rlcp;
 
     void build_sa() {
-        //TODO
-    }
-
-    void build_lcp(int b, int e) {
-        //TODO
+        this->sa = (int*) malloc(this->n);
+        for(int i = 0; i < n; ++i) sa[i] = i;
+        sort(sa, sa+n, cmp);
+        for(int i = 0; i < n; ++i) printf("%d %d %s\n", i, sa[i], T+sa[i]);
     }
 
     int computeLCP(char* str1, char* str2) {
         int i;
         for(i = 0; str1[i] && str2[i] && str1[i] == str2[i]; ++i);
-        return i-1;
+        return i;
+    }
+
+    void build_lcp(int b, int e) {
+        if(e-b <= 1) return;
+        int mid = (b+e)/2;
+        printf("%d %d %d\n", b, mid, e);
+        printf("before %d %d %d\n", sa[b], sa[mid], sa[e]);
+        Llcp[mid] = computeLCP(T+sa[b], T+sa[mid]);
+        Rlcp[mid] = computeLCP(T+sa[e], T+sa[mid]);
+        printf("after %d %d %d\n", sa[b], sa[mid], sa[e]);
+        build_lcp(b, mid);
+        build_lcp(mid, e);
     }
 
     int computeLW(char* W, int p) {
         int Lw;
+        //printf("%d %d %s\n", n-1, sa[n-1], T+sa[n-1]);
         int l = computeLCP(T+sa[0], W);
         int r = computeLCP(T+sa[n-1], W);
+        //printf("%d %d %s\n", n-1, sa[n-1], T+sa[n-1]);
         if(l == p || W[l] <= T[sa[0]+l])
             Lw = 0;
-        else if(r < p || W[r] <= T[sa[n-1]+r])
+        else if(r < p && W[r] > T[sa[n-1]+r])
             Lw = n;
         else {
             int L = 0, R = n-1;
+            //cout << "oi" << endl;
             while(R-L > 1) {
                 int M = (L+R)/2;
                 int m;
@@ -64,27 +84,43 @@ private:
         //TODO
     }
 
+    
+
 public:
     SuffixArray(char* T, int n) {
-        this->T = (char*) malloc(n);
+        this->T = (char*) malloc(n+2);
         strcpy(this->T, T);
         this->n = n;
+        strcpy(A, this->T);
         build_sa();
+        //cout << n << endl;
         this->Llcp = (int*) malloc(n);
         this->Rlcp = (int*) malloc(n);
+        //printf("%s\n", T+5);
+        cout << sa[n-1] << endl;
         build_lcp(0, n-1);
+        cout << sa[n-1] << endl;
     }
 
-    pair<int,int> findPattern(char* W, int p) {
-        int Lw = computeLW(W, p);
+    int findPattern(char* W, int p) {
+        /*int Lw = computeLW(W, p);
         int Rw = computeRW(W, p);
-        return make_pair(Lw, Rw);
+        return make_pair(Lw, Rw);*/
+        return computeLW(W, p);
     }
+
 };
 
 int main(){
-    char T[100];
+    char T[100], W[100];
     scanf("%s", T);
     SuffixArray sa = SuffixArray(T, strlen(T));
+    int q; scanf("%d", &q);
+    while(q--) {
+        scanf("%s", W);
+        //sa.call(T, W);
+        //printf("%s %s\n", T, W);
+        printf("%d\n", sa.findPattern(W, strlen(W)));
+    }
     return 0;
 }
